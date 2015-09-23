5becf9828a8235ba1d498de7df67fcebae1379e0
capawaky <capawaky@gmail.com>
2015-09-23 09:25:04 +0300

    Mouseover title text added

--- instagram_more.js
+++ instagram_more.js
@@ -13,10 +13,27 @@ $(document).ready(function(){
         $("#largePhoto").attr("src", data.data[0].images.standard_resolution.url ); 
         $("#photoLink").attr("href", data.data[0].link );
         $("#photoLink").attr("title", data.data[0].caption.text );
+        
         // tooltip from http://www.alessioatzeni.com/blog/simple-tooltip-with-jquery-only-text/ to display Instagram caption text
         $("#photoLink").hover(function(){
-            var title = $(this).attr('title')
-        })
+            var title = $(this).attr('title');
+            $(this).data('tipText', title).removeAttr('title');
+            $('<p class="tooltip"></p>')
+            .text(title)
+            .appendTo('body')
+            .fadeIn('slow');
+
+        }, function () {
+            $('this').attr('title', $(this).data('tipText'));
+            $('.tooltip').remove();
+            $("#photoLink").attr("title", data.data[0].caption.text );
+
+        }).mousemove(function(e){
+            var mousex = e.pageX + 10; 
+            var mousey = e.pageY + 10;
+            $('.tooltip')
+            .css({ top: mousey, left: mousex })
+        });
         // loops to generate four thumbnail photos
         start = 0; 
         end = 3;
