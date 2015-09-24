a12e4696ebd15747f852c7fcbc6e367728b39783
capawaky <capawaky@gmail.com>
2015-09-24 12:37:41 +0300

    Cleaned code up

--- instagram_more.js
+++ instagram_more.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // uses https://instagram.com/developer/endpoints/users/#get_users_media_recent to get 4 most recent pictures from Instagram. requires a registered client & permission from user
     var url = "https://api.instagram.com/v1/users/1551735546/media/recent/?access_token=1551735546.4bf074c.c50158944161461cb019d912fa816df9" 
-    var images, index, i;
+    var instagramData, index, i;
     $.ajax({
 		type: 'GET',
 		url: url,
@@ -9,10 +9,11 @@ $(document).ready(function(){
 		dataType: 'jsonp'
 })
 	.done(function(data){
+        instagramData = data.data; 
         // loads initial main photo, URL to Instagram and Instagram caption text
-        $("#largePhoto").attr("src", data.data[0].images.standard_resolution.url ); 
-        $("#photoLink").attr("href", data.data[0].link );
-        $("#photoLink").attr("title", data.data[0].caption.text );
+        $("#largePhoto").attr("src", instagramData[0].images.standard_resolution.url ); 
+        $("#photoLink").attr("href", instagramData[0].link );
+        $("#photoLink").attr("title", instagramData[0].caption.text );
         // tooltip by Alessio Atzeni (http://www.alessioatzeni.com/blog/simple-tooltip-with-jquery-only-text/) to display Instagram caption text when hovering
         $("#photoLink").hover(function(){
             var title = $(this).attr('title');
@@ -25,7 +26,7 @@ $(document).ready(function(){
         }, function () {
             $('this').attr('title', $(this).data('tipText'));
             $('.tooltip').remove();
-            $("#photoLink").attr("title", data.data[0].caption.text );
+            $("#photoLink").attr("title", instagramData[0].caption.text );
         // captures mouse position on screen 
         }).mousemove(function(e){
             var mousex = e.pageX + 10; 
@@ -33,45 +34,25 @@ $(document).ready(function(){
             $('.tooltip')
             .css({ top: mousey, left: mousex })
         });
-        // loops to generate four thumbnail photos
 
         index = 0; 
-        images = data.data; 
+        // loops to generate four thumbnail photos
         for (i = 0; i <= 3; i++){ 
-        $("#"+i).attr("src", images[i].images.thumbnail.url )
+        $("#"+i).attr("src", instagramData[i].images.thumbnail.url )
         }
-        // var index increases with each click, shows older thumbnails
-        
-
-
-
+        // clicks on navigation buttons reduce or increase index number to receive a new array
         $(".navigation").click(function(){
            if ($(this).attr("id") == $("#oldPhoto").prop("id") && index < 16) {
             ++index; 
-            //console.log($(this).attr("id") + " " + $("#oldPhoto").prop("id"))
            } else if ($(this).attr("id") == $("#newPhoto").prop("id") && index >= 1) {
             --index;
-            console.log("New photo this:" + $(this))
            }
             for (i = 0; i <= 3; i++){ 
-                $("#"+i).attr("src", images[i+index].images.thumbnail.url )
+                $("#"+i).attr("src", instagramData[i+index].images.thumbnail.url )
             }
         })
-
-
-
-
-
-        // var index decreases with each click, shows newer thumbnails 
-        // $("#newPhoto").click(function(){
-        //     if (index >= 1){
-        //         --index;
-        //      }
-        //     for (i = 0; i <= 3; i++){ 
-        //         $("#"+i).attr("src", images[i+index].images.thumbnail.url )
-        //     }
-        // })
-
+        
+        // thumbnail mouse hover
         $("#thumbs img").hover(function (){   
         // hover over thumbnails launches effects              
             $("#largePhoto").fadeOut(300);                  
@@ -81,9 +62,9 @@ $(document).ready(function(){
             setTimeout(function(){
                         console.log("index number:" + index)
                         console.log("This is id: " + $(elemId).attr("id"))
-                $("#largePhoto").attr("src", data.data[parseInt($(elemId).attr("id"))+ index ].images.standard_resolution.url);
-                $("#photoLink").attr("href", data.data[parseInt($(elemId).attr("id"))+ index ].link);
-                $("#photoLink").attr("title", data.data[parseInt($(elemId).attr("id"))+ index ].caption.text);
+                $("#largePhoto").attr("src", instagramData[parseInt($(elemId).attr("id"))+ index ].images.standard_resolution.url);
+                $("#photoLink").attr("href", instagramData[parseInt($(elemId).attr("id"))+ index ].link);
+                $("#photoLink").attr("title", instagramData[parseInt($(elemId).attr("id"))+ index ].caption.text);
                 }, 300);
             },  function(){
             // when the cursors leaves a thumbnail, stops the animation and shows the main photo
